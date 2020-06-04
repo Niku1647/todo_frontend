fb13c266642718fa6be4352c7b7ee5adff4a69ed
Subhakanta Pati <59618850+Niku1647@users.noreply.github.com>
2020-06-05 01:27:08 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,49 +3,30 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  
+
   const [name, setName]=useState([])
+
   const increment =  () =>{
-    
-    let x = document.getElementById('text').value
-    
-    setName([...name,x])
-    
-    
+     let x = document.getElementById('text').value
+     setName([...name,x])
+    }
 
-  }
   const remove = (e) =>{
-    
-      
-      
-       
+    const arr = name.filter(item => item !== name[parseInt(e.target.name)] )
+    setName(arr)
+    }
 
-       const arr = name.filter(item => item !== name[parseInt(e.target.name)] )
-
-       setName(arr)
-
-
-      
-  
-    
-    
-
-  }
   return (
     <div className="App">
-      <textarea id="text"></textarea>
-     <button onClick={increment}>Click Me</button>
-  <ul>{name.map((item,i)=>{
-    return <div  key={i} >
-      <li >{item}</li>
-      <button name={i} onClick={remove}>Remove</button>
-      </div>
-  })}
-       </ul>
-
-        
-      
-       
+      <textarea id="text"></textarea><br />
+      <button onClick={increment}>Click Me</button>
+      <ul>{name.map((item,i)=>{
+          return <div key={i}>
+                    <p>{item}</p>
+                    <button name={i} onClick={remove}>Remove</button>
+                 </div>
+           })}
+      </ul>
     </div>
   );
 }
